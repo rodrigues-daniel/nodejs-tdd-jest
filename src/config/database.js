91e5fd7a34cb25bbd91b408c22711e80bb7c0089
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2021-08-25 14:37:30 -0300

    add sqlite database

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,9 @@ require("dotenv").config({
 });
 
 module.exports = {
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,  
   database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT || "postgres",
+  dialect: process.env.DB_DIALECT || "sqlite",
   storage: "./__tests__/database.sqlite",
   operatorsAliases: false,
   logging: false,
